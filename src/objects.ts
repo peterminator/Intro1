c038b4f2a76f161d5469d4414a7007d31b88165c
peterminator <peterminator@gmail.com>
2022-11-30 12:57:00 +0100

    types update

--- src/objects.ts
+++ src/objects.ts
@@ -2,6 +2,7 @@ let aa :object = {
     b:'x'
 }
 
+console.log ("object type", aa)
 // error accesing oibject 
 //console.log(aa.b)
 
@@ -32,3 +33,85 @@ class Person {
 bb3 = new Person("Pedro", "Familiar")
 
 console.log ("Object asign to constructor", bb3)
+
+/*
+Error
+
+let a : {b:number}
+
+a = {} // error!
+
+a = {b:1, c:3} // error
+
+
+*/
+
+// optional propertys and index signatures
+console.log("objects::optional properties")
+console.log("--------------------------------------------------")
+
+let bb4 :{
+    b:number,
+    c?: string,
+    [key:number]: boolean
+}
+
+bb4 = {b:1}
+console.log("only b defined", bb4)
+
+if (bb4.c === undefined) {
+    console.log ("c is undefined without defined it")
+}
+
+
+bb4 = {b:1, c:undefined}
+console.log("only c undefined", bb4)
+bb4 = {b:1, c:"valuec"}
+console.log("only c with value", bb4)
+bb4 = {b:1, c:"valuec"}
+console.log("only c with value", bb4)
+bb4 = {b:1, 10:false, 30:true}
+console.log("only c with value", bb4)
+
+
+
+let bb5 :{    
+    [key:string]: string
+}
+
+bb5 = {}
+bb5  ["paste"] = "mal"
+bb5  ["calste"] = "bien"
+
+console.log("keys with string", bb5)
+
+// read only
+let bb6 :{
+    readonly firstName:string
+} = {
+    firstName : "peter"
+
+
+}
+
+// type aliases
+type Age = number
+
+type Person1 = {
+    name:string,
+    age:Age
+}
+
+let iam :Person1 = {
+    name:"peter",
+    age:33
+}
+
+type Returns = string | null
+function trueOrNull (isTrue:boolean):Returns {
+    if (isTrue) {
+        return "true"
+    }
+
+    return null    
+}
