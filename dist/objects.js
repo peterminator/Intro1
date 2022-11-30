a632e2aecd457d8762b9b87e66e26ac92f390077
peterminator <peterminator@gmail.com>
2022-11-30 11:12:42 +0100

    add object code

--- dist/objects.js
+++ dist/objects.js
@@ -5,7 +5,23 @@ let aa = {
 // error accesing oibject 
 //console.log(aa.b)
 let bb = {
-    b: "string"
+    b: "string value"
 };
 console.log("object", bb.b);
+let bb1 = {
+    b: 12
+};
+console.log("object with number", bb1.b);
+let bb3 = {
+    firstName: "John",
+    lastName: "barrowman"
+};
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+bb3 = new Person("Pedro", "Familiar");
+console.log("Object asign to constructor", bb3);
 //# sourceMappingURL=objects.js.map
--- src/objects.ts
+++ src/objects.ts
@@ -11,3 +11,24 @@ let bb={
 
 console.log ("object", bb.b)
 
+let bb1 :{b:number} = {
+    b:12
+}
+
+console.log ("object with number", bb1.b)
+
+let bb3 = {
+    firstName:"John",
+    lastName:"barrowman"
+}
+
+class Person {
+    constructor (
+        public firstName: string,
+        public lastName:string   )
+        {}    
+}
+
+bb3 = new Person("Pedro", "Familiar")
+
+console.log ("Object asign to constructor", bb3)
